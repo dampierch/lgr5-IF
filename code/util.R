f3e0bbf0199e597dd28aa5b714539eb428ec4812
dampierch <odampso@yahoo.com>
2021-02-01 23:43:30 -0500

    add sjp counts, update util

--- code/util.R
+++ code/util.R
@@ -7,15 +7,14 @@ read_inputs <- function() {
     data$crypts <- readr::read_tsv(target)
     target <- paste0(data_dir, "crypt_counts_blind_chd.tsv")
     data$counts_chd <- readr::read_tsv(target)
+    target <- paste0(data_dir, "crypt_counts_blind_sjp.tsv")
+    data$counts_sjp <- readr::read_tsv(target)
     return(data)
 }
 
 
-make_data <- function() {
-    data <- read_inputs()
-    df <- merge(data$crypts, data$subjects, by="Subject_ID")
-    df <- merge(df, data$counts_chd, by=c("Crypt_ID", "Slide_Number"))
-    dfsum <- df %>% dplyr::group_by(Subject_ID) %>%
+sum_df <- function(df) {
+    df1 <- df %>% dplyr::group_by(Subject_ID) %>%
         dplyr::summarise(
             Number=as.numeric(substring(dplyr::first(Subject_ID), 2)),
             ID=dplyr::first(Subject_ID),
@@ -29,5 +28,27 @@ make_data <- function() {
             Ectopic_Mean=mean(Ectopic_Count),
             Ectopic_SD=sd(Ectopic_Count)
         ) %>% dplyr::arrange(desc(Diagnosis), Number)
-    return(setNames(list(df, dfsum), c("data", "summary")))
+    return(df1)
+}
+
+
+make_data <- function() {
+    data <- read_inputs()
+    dfs <- list()
+    dfs$full <- merge(data$crypts, data$subjects, by="Subject_ID")
+    fnames <- c("Crypt_ID", "Slide_Number")
+    dfs$full <- merge(dfs$full, data$counts_chd, by=fnames)
+    snames <- c("_A", "_B")
+    dfs$full <- merge(dfs$full, data$counts_sjp, by=fnames, suffixes=snames)
+    #dfs$full <- merge(dfs$full, data$counts_ltj, by=fnames)
+    i <- !is.na(dfs$full$LGR5_Count_A) & !is.na(dfs$full$LGR5_Count_B)
+    dfs$lgr5_ttl <- dfs$full[i, ]
+    i <- !is.na(dfs$full$Ectopic_Count_A) & !is.na(dfs$full$Ectopic_Count_B)
+    dfs$lgr5_ect <- dfs$full[i, ]
+
+    dfs$lgr5_ttl$LGR5_Count <- (dfs$lgr5_ttl$LGR5_Count_A + dfs$lgr5_ttl$LGR5_Count_B) / 2
+
+    dfs$sum_ttl <- sum_df(dfs$lgr5_ttl)
+    dfs$sum_ect <- sum_df(dfs$lgr5_ect)
+    return(dfs)
 }
